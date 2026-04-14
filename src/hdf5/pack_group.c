6827c5025b2555ccf1ce30259c7bfccc969d515c
John Helly <j.c.helly@durham.ac.uk>
2026-04-14 16:56:54 +0100

    Modify C code to avoid recursion into large groups

--- src/hdf5/pack_group.c
+++ src/hdf5/pack_group.c
@@ -15,6 +15,9 @@
 #error "HDF5 version 1.12.0 or newer is required."
 #endif
 
+/* If a group contains too many links, just return the link names */
+#define MAX_LINKS_FOR_RECURSION 20
+
 /*
   Recursively pack a HDF5 group and its members to the supplied msgpack
   packer, subject to a maximum recursion depth. max_depth=0 means only
@@ -53,8 +56,11 @@ int pack_group(hid_t obj_id, msgpack_packer pk, int max_depth,
   if(H5Gget_info(obj_id, &ginfo) < 0)goto cleanup;
   int nr_links = ginfo.nlinks;
 
-  /* Get metadata for group members and count how many we can encode */
-  int nr_members = 0;
+  /* Determine whether we try to encode member objects */
+  int encode_members = 1;
+  if((nr_links > MAX_LINKS_FOR_RECURSION) || (max_depth <= 0))encode_members = 0;
+
+  /* Get metadata for group members */
   oinfo = malloc(sizeof(H5O_info2_t)*nr_links);
   if(!oinfo)goto cleanup;
   linfo = malloc(sizeof(H5L_info2_t)*nr_links);
@@ -65,20 +71,19 @@ int pack_group(hid_t obj_id, msgpack_packer pk, int max_depth,
     switch(linfo[link_nr].type) {
     case H5L_TYPE_HARD:
     case H5L_TYPE_EXTERNAL:
-      /* Get object info for hard or external links */
-      if(H5Oget_info_by_idx3(obj_id, ".", H5_INDEX_NAME, H5_ITER_NATIVE, link_nr, &oinfo[link_nr], H5O_INFO_BASIC, H5P_DEFAULT) < 0)goto cleanup;
-      /* Objects which are not groups or datasets will be skipped */
-      if((oinfo[link_nr].type == H5O_TYPE_GROUP) || (oinfo[link_nr].type == H5O_TYPE_DATASET))nr_members += 1;
+      if(encode_members) {
+        /* Get object info for hard or external links */
+        if(H5Oget_info_by_idx3(obj_id, ".", H5_INDEX_NAME, H5_ITER_NATIVE, link_nr, &oinfo[link_nr], H5O_INFO_BASIC, H5P_DEFAULT) < 0)goto cleanup;
+      }
       break;
     case H5L_TYPE_SOFT:
       /* We serialize soft link paths without dereferencing them */
-      nr_members += 1;
       break;
     case H5L_TYPE_ERROR:
       /* Something went wrong */
       goto cleanup;
     default:
-      /* Skip unknown link types */
+      /* Nothing to do for unknown link types */
       break;
     }
   }
@@ -100,7 +105,7 @@ int pack_group(hid_t obj_id, msgpack_packer pk, int max_depth,
   /* Pack members of this group */
   check(msgpack_pack_str(&pk, 7));
   check(msgpack_pack_str_body(&pk, "members", 7));
-  check(msgpack_pack_map(&pk, nr_members));
+  check(msgpack_pack_map(&pk, nr_links));
   for(int link_nr=0; link_nr<nr_links; link_nr+=1) {
     /* Get name of this group member */
     ssize_t len = H5Lget_name_by_idx(obj_id, ".", H5_INDEX_NAME, H5_ITER_NATIVE, link_nr, NULL, 0, H5P_DEFAULT);
@@ -111,28 +116,38 @@ int pack_group(hid_t obj_id, msgpack_packer pk, int max_depth,
     /* Add name to the map */
     check(msgpack_pack_str(&pk, len));
     check(msgpack_pack_str_body(&pk, name, len));
-    /* Now we need to pack the member object */
-    switch(linfo[link_nr].type) {
-    case H5L_TYPE_HARD:
-    case H5L_TYPE_EXTERNAL:
-      /* If it's a hard or external link to a group or dataset, pack it. */
-      if((oinfo[link_nr].type == H5O_TYPE_GROUP) || (oinfo[link_nr].type == H5O_TYPE_DATASET)) {
-        check(pack_object(obj_id, name, pk, max_depth-1, data_size_limit, buffer_size));
+    /* Now pack either the member object or a nil */
+    if(encode_members) {
+      /* Now we need to pack the member object */
+      switch(linfo[link_nr].type) {
+      case H5L_TYPE_HARD:
+      case H5L_TYPE_EXTERNAL:
+        /* If it's a hard or external link to a group or dataset, pack it. */
+        if((oinfo[link_nr].type == H5O_TYPE_GROUP) || (oinfo[link_nr].type == H5O_TYPE_DATASET)) {
+          check(pack_object(obj_id, name, pk, max_depth-1, data_size_limit, buffer_size));
+        } else {
+          /* Link to an unknown object type */
+          check(msgpack_pack_nil(&pk));
+        }
+        break;
+      case H5L_TYPE_SOFT:
+        /* This is a soft link, so we'll just store the path to the object */
+        check(pack_soft_link(obj_id, name, pk, &linfo[link_nr]));
+        break;
+      case H5L_TYPE_ERROR:
+        /* Something went wrong */
+        goto cleanup;
+      default:
+        /* Unknown link type */
+        check(msgpack_pack_nil(&pk));
+        break;
       }
-      break;
-    case H5L_TYPE_SOFT:
-      /* This is a soft link, so we'll just store the path to the object */
-      check(pack_soft_link(obj_id, name, pk, &linfo[link_nr]));
-      break;
-    case H5L_TYPE_ERROR:
-      /* Something went wrong */
-      goto cleanup;
-    default:
-      /* Skip unknown link types */
-      break;
+      free(name);
+      name = NULL;
+    } else {
+      /* We're not encoding the member objects, so just send a nil */
+      check(msgpack_pack_nil(&pk));
     }
-    free(name);
-    name = NULL;
   }
 
   /* Success */
