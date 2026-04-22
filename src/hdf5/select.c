3f71a01987cc902b4bb586a4103716d229214f01
John Helly <j.c.helly@durham.ac.uk>
2026-04-22 20:55:28 +0100

    Try to avoid leaking dataspace IDs

--- src/hdf5/select.c
+++ src/hdf5/select.c
@@ -21,16 +21,19 @@ static hid_t select_slices_recursive(hid_t file_space_id,
 
   hid_t space_id_left  = -1;
   hid_t space_id_right = -1;
-  hid_t combined = -1;
-  hid_t result = H5Scopy(file_space_id);
+  hid_t result = -1;
   if(nr_selections == 0) {
 
     /* No selections, so select nothing */
+    result = H5Scopy(file_space_id);
+    if(result < 0)goto cleanup;
     if(H5Sselect_none(result) >= 0)return result;
 
   } else if(nr_selections == 1) {
 
     /* One selection, so select it */
+    result = H5Scopy(file_space_id);
+    if(result < 0)goto cleanup;
     start[0] = select_start[first_selection];
     count[0] = select_count[first_selection];
     if(H5Sselect_hyperslab(result, H5S_SELECT_SET, start, NULL, count, NULL) >= 0)return result;
@@ -46,16 +49,15 @@ static hid_t select_slices_recursive(hid_t file_space_id,
     if(space_id_left < 0)goto cleanup;
     space_id_right = select_slices_recursive(file_space_id, first_on_right, nr_right, select_start, select_count, start, count);
     if(space_id_right < 0)goto cleanup;
-    combined = H5Scombine_select(space_id_left, H5S_SELECT_OR, space_id_right);
-    if(combined < 0)goto cleanup;
+    result = H5Scombine_select(space_id_left, H5S_SELECT_OR, space_id_right);
+    if(result < 0)goto cleanup;
     H5Sclose(space_id_left);
     H5Sclose(space_id_right);
-    return combined;
+    return result;
   }
 
  cleanup:
   if(result >= 0)H5Sclose(result);
-  if(combined >= 0)H5Sclose(combined);
   if(space_id_left >= 0)H5Sclose(space_id_left);
   if(space_id_right >= 0)H5Sclose(space_id_right);
   return -1;
@@ -69,5 +71,7 @@ herr_t select_slices(hid_t file_space_id, hsize_t nr_selections,
   hid_t dspace_id = select_slices_recursive(file_space_id, 0, nr_selections,
                                             select_start, select_count, start, count);
   if(dspace_id < 0)return -1;
-  return H5Sselect_copy(file_space_id, dspace_id);
+  herr_t err = H5Sselect_copy(file_space_id, dspace_id);
+  H5Sclose(dspace_id);
+  return err;
 }
