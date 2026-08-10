c11a9c1a9ff3ed75fc506b8a906a0988a34cd871
John Helly <j.c.helly@durham.ac.uk>
2026-08-10 17:54:28 +0100

    Add functions to count unique users

--- webapp/src/main/java/uk/ac/dur/cosma/hdfstream/RequestCounter.java
+++ webapp/src/main/java/uk/ac/dur/cosma/hdfstream/RequestCounter.java
@@ -5,6 +5,9 @@ import java.time.LocalDate;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.Collections;
+
 
 public class RequestCounter {
 
@@ -14,6 +17,7 @@ public class RequestCounter {
 
     private final ArrayList<ConcurrentHashMap<Long,LongAdder>> nrRequests = new ArrayList<ConcurrentHashMap<Long,LongAdder>>();
     private final ArrayList<ConcurrentHashMap<Long,LongAdder>> nrBytes = new ArrayList<ConcurrentHashMap<Long,LongAdder>>();
+    private final ConcurrentHashMap<Long, Set> uniqueUsers = new ConcurrentHashMap<Long, Set>();
 
     public RequestCounter() {
         for(int i=0; i<3; i+=1) {
@@ -22,18 +26,23 @@ public class RequestCounter {
         }
     }
 
-    private long currentBucket() {
+    private long currentDay() {
         return LocalDate.now().toEpochDay();
     }
 
     public void logRequest(int kind, long size) {
-        long now = currentBucket();
+        long now = currentDay();
         nrRequests.get(kind).computeIfAbsent(now, k -> new LongAdder()).increment();
         nrBytes.get(kind).computeIfAbsent(now, k -> new LongAdder()).add(size);
     }
 
+    public void logUser(String username) {
+        long now = currentDay();
+        uniqueUsers.computeIfAbsent(now, k -> Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>())).add(username);
+    }
+
     public long getCount(int kind, int nrDays) {
-        long now = currentBucket();
+        long now = currentDay();
         long cutoff = now - nrDays + 1;
         return nrRequests.get(kind).entrySet().stream().filter(e -> e.getKey() >= cutoff).mapToLong(e -> e.getValue().sum()).sum();
     }
@@ -43,12 +52,22 @@ public class RequestCounter {
     }
 
     public long getBytes(int kind, int nrDays) {
-        long now = currentBucket();
+        long now = currentDay();
         long cutoff = now - nrDays + 1;
         return nrBytes.get(kind).entrySet().stream().filter(e -> e.getKey() >= cutoff).mapToLong(e -> e.getValue().sum()).sum();
     }
 
     public long getBytes(int kind) {
         return getBytes(kind, 36500);
+    }
+
+    public int getUserCount(int daysAgo) {
+        Set<String> users = uniqueUsers.get(currentDay()-daysAgo);
+        if(users == null) {
+            // No recorded requests on this day
+            return 0;
+        } else {
+            return users.size();
+        }
     }
 }
