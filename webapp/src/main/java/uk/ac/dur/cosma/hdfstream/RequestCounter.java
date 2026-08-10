fa95eaa8a9086c50695901d93e31e0f083e650c3
John Helly <j.c.helly@durham.ac.uk>
2026-08-10 17:35:26 +0100

    Use concurrent hash map instead of cache for counters

--- webapp/src/main/java/uk/ac/dur/cosma/hdfstream/RequestCounter.java
+++ webapp/src/main/java/uk/ac/dur/cosma/hdfstream/RequestCounter.java
@@ -1,13 +1,10 @@
 package uk.ac.dur.cosma.hdfstream;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.concurrent.atomic.LongAdder;
-import java.util.HashMap;
-
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
 
 public class RequestCounter {
 
@@ -15,19 +12,13 @@ public class RequestCounter {
     public static final int DIRECTORY = 1;
     public static final int MSGPACK = 2;
 
-    // Keep counts for the last MAX_DAYS days
-    private static int MAX_DAYS = 7;
-    private final HashMap<Integer,Cache<Long,LongAdder>> nrRequests = new HashMap<Integer,Cache<Long,LongAdder>>();
-    private final HashMap<Integer,Cache<Long,LongAdder>> nrBytes = new HashMap<Integer,Cache<Long,LongAdder>>();
+    private final ArrayList<ConcurrentHashMap<Long,LongAdder>> nrRequests = new ArrayList<ConcurrentHashMap<Long,LongAdder>>();
+    private final ArrayList<ConcurrentHashMap<Long,LongAdder>> nrBytes = new ArrayList<ConcurrentHashMap<Long,LongAdder>>();
 
     public RequestCounter() {
         for(int i=0; i<3; i+=1) {
-            nrRequests.put(i, Caffeine.newBuilder()
-                           .expireAfterWrite(Duration.ofDays(MAX_DAYS))
-                           .build());
-            nrBytes.put(i, Caffeine.newBuilder()
-                        .expireAfterWrite(Duration.ofDays(MAX_DAYS))
-                        .build());
+            nrRequests.add(new ConcurrentHashMap<Long,LongAdder>());
+            nrBytes.add(new ConcurrentHashMap<Long,LongAdder>());
         }
     }
 
@@ -36,23 +27,28 @@ public class RequestCounter {
     }
 
     public void logRequest(int kind, long size) {
-        nrRequests.get(kind).get(currentBucket(), k -> new LongAdder()).increment();
-        nrBytes.get(kind).get(currentBucket(), k -> new LongAdder()).add(size);
+        long now = currentBucket();
+        nrRequests.get(kind).computeIfAbsent(now, k -> new LongAdder()).increment();
+        nrBytes.get(kind).computeIfAbsent(now, k -> new LongAdder()).add(size);
+    }
+
+    public long getCount(int kind, int nrDays) {
+        long now = currentBucket();
+        long cutoff = now - nrDays + 1;
+        return nrRequests.get(kind).entrySet().stream().filter(e -> e.getKey() >= cutoff).mapToLong(e -> e.getValue().sum()).sum();
     }
 
     public long getCount(int kind) {
-        return nrRequests.get(kind).asMap()
-            .values()
-            .stream()
-            .mapToLong(LongAdder::sum)
-            .sum();
+        return getCount(kind, 36500); // Last 100 years!
+    }
+
+    public long getBytes(int kind, int nrDays) {
+        long now = currentBucket();
+        long cutoff = now - nrDays + 1;
+        return nrBytes.get(kind).entrySet().stream().filter(e -> e.getKey() >= cutoff).mapToLong(e -> e.getValue().sum()).sum();
     }
 
     public long getBytes(int kind) {
-        return nrBytes.get(kind).asMap()
-            .values()
-            .stream()
-            .mapToLong(LongAdder::sum)
-            .sum();
+        return getBytes(kind, 36500);
     }
 }
